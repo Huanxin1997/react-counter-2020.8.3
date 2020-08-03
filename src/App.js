07ba66bbe24faf5b1dbd595237e9ddd2b24ce4f7
Hans <741581879@qq.com>
2020-08-03 15:35:30 +0800

    [Counter][Hans] feature: First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CounterGroup from './component/CounterGroup/Index'
+// import Calculator from './component/calculator/Index'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CounterGroup className="center_box" />
+    </div>
+  );
+}
+
+export default App;
